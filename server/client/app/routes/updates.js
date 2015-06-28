dde25bff2faf1726b63b5d983b6dea0fc142d7eb
Robert Xu <robxu9@gmail.com>
2015-06-28 08:24:30 -0700

    ember updates

--- server/client/app/routes/updates.js
+++ server/client/app/routes/updates.js
@@ -1,8 +1,11 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-	titleToken: "Latest Updates",
-	model: function() {
-		return Ember.$.getJSON("/api/v1/updates");
-	}
+  titleToken: "Latest Updates",
+  model: function() {
+    return Ember.RSVP.hash({
+      updates: Ember.$.getJSON("/api/v1/updates"),
+      targets: Ember.$.getJSON("/api/v1/updates/targets")
+    });
+  }
 });
